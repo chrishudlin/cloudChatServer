2c799c9a4aae884f3de40d440987d87d9ed7e03d
Elliott Hill <direct@mrhilster.com>
2017-10-24 12:13:24 +0200

    Unique users and disconnect message attempt
    Non-working state, it appears to work but the users browser for some reason refreshes after inputting the users name.

--- Index.js
+++ Index.js
@@ -1,21 +1,34 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  var u;
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-  socket.on('connection message', function(msg){
-    io.emit('connection message', msg);
+  socket.on('user given', function(userGiven){
+    if(users.indexOf(userGiven) > -1) {
+      socket.emit('user taken');
+    }else{
+      u = userGiven;
+      users.push(u);
+      console.log(u + ' connected');
+      socket.emit('connection success');
+      io.emit('new user connected', u);
+    }
   });
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    if(u != null){
+      users.pop(u);
+      io.emit('user disconnected', u);
+      console.log(u + ' disconnected');
+    }
   });
 });
 
