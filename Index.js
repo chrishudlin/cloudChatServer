dfc931d763ee0200c351d0f01bd0e28d03395429
Elliott Hill <direct@mrhilster.com>
2017-10-26 15:36:56 +0200

    Private messaging and list
    Implemented private messaging to specific users as well as aquiring a list of users that are currently online.

--- Index.js
+++ Index.js
@@ -13,17 +13,27 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
   socket.on('user connected', function(userGiven){
+    u = userGiven;
+    users.push(u);
+    io.emit('connection message', u + " has connected to the chat");
+    console.log(u + ' connected');
+  });
+  socket.on('get users', function(callback){
+    callback(users);
+  });
+  socket.on('get user for private chat', function(userGiven, callback){
     if(users.indexOf(userGiven) > -1){
-      //socket.emit('connection unsuccessful');
+      callback(true);
     }else{
-      u = userGiven;
-      //socket.emit('connection successful', u);
-      io.emit('connection message', userGiven + " has connected to the chat");
-      console.log(userGiven + ' connected');
+      callback(false);
     }
+  });
+  socket.on('private chat message', function(userGiven, msg){
+    io.emit('private chat message', userGiven, msg);
   });
   socket.on('disconnect', function(){
     if(u != ""){
+      users.pop(u);
       io.emit('disconnection message', u + " has disconnected to the chat");
       console.log(u + ' disconnected');
     }
